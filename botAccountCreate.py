0338938c4ac257bd5f456b3d7e16d4b76edf74c1
Mogan07 <65966797+Mogan07@users.noreply.github.com>
2020-06-23 13:47:21 +0100

    Add files via upload

--- botAccountCreate.py
+++ botAccountCreate.py
@@ -4,8 +4,8 @@ import time
 from selenium.webdriver.common.by import By
 import accountInfoGenerator as account
 
-browser= webdriver.Chrome("chrome web driver path here")
-browser.get("http://www.instagram.com")
+browser= webdriver.Chrome("C:\chromedriver\chromedriver.exe")  #download chromedriver after create a new folder in C name it "chromedriver" then place the chromedriver there!
+browser.get("https://www.instagram.com/accounts/emailsignup/")
 time.sleep(8) #time.sleep count can be changed depending on the Internet speed.
 name = account.username()
 
@@ -26,7 +26,7 @@ print(name)
 password_field  = browser.find_element_by_name('password')
 password_field.send_keys('aa12345bb12345cc'+name) #You can determine another password here.
 
-submit = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[6]/span/button')
+submit = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[7]/div/button')
 submit.click()
 time.sleep(8)
 
